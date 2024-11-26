b83097bfbc253fce8e16ed2e5afd1daf304cd282
Lee <107117136+leepotatoo@users.noreply.github.com>
2024-11-25 23:11:18 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -48,12 +48,13 @@ async function quantidadeUsuariosPorRede() {
                 }
             }
         }
+        
     }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
