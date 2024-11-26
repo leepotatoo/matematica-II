9f056d3b5ba3d9eb6201767cb699bd0fe7149fea
Lee <107117136+leepotatoo@users.noreply.github.com>
2024-11-25 23:41:47 -0300

    Update redesSociaisMinhaEscola.js

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -1,36 +1,39 @@
-import { criarGrafico, getCSS, incluirTexto } from "./common.js"
+import { criarGrafico, getCSS, incluirTexto } from "./common.js";
 
 async function redesSociaisFavoritasMinhaEscola() {
-    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+    const dadosLocaisString = localStorage.getItem('respostaRedesSociais');
     if (dadosLocaisString) {
-        const dadosLocais = JSON.parse(dadosLocaisString)
-        processarDados(dadosLocais)
+        const dadosLocais = JSON.parse(dadosLocaisString);
+        processarDados(dadosLocais);
     } else {
-        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
-        const res = await fetch(url)
-        const dados = await res.json()
-        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
-        processarDados(dados)
+        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF';
+        const res = await fetch(url);
+        const dados = await res.json();
+        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados));
+        processarDados(dados);
     }
 }
 
 function processarDados(dados) {
-    const redesSociais = dados.slice(1).map(redes => redes[1])
+    const redesSociais = dados.slice(1).map(redes => redes[1]);
     const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
-        acc[redesSociais] = (acc[redesSociais] || 0) + 1
-        return acc
-    }, {})
-    const valores = Object.values(contagemRedesSociais)
-    const labels = Object.keys(contagemRedesSociais)
+        acc[redesSociais] = (acc[redesSociais] || 0) + 1;
+        return acc;
+    }, {});
+    const valores = Object.values(contagemRedesSociais);
+    const labels = Object.keys(contagemRedesSociais);
 
     const data = [
         {
             values: valores,
             labels: labels,
             type: 'pie',
-            textinfo: 'label+percent'
+            textinfo: 'label+percent',
+            marker: {
+                colors: ['#FF5733', '#33FF57', '#3357FF', '#F3FF33', '#FF33F3'] // Paleta de cores personalizada
+            }
         }
-    ]
+    ];
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
@@ -51,10 +54,10 @@ function processarDados(dados) {
                 size: 16
             }
         }
-    }
+    };
 
-    criarGrafico(data, layout)
-    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+    criarGrafico(data, layout);
+    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`);
 }
 
-redesSociaisFavoritasMinhaEscola()
+redesSociaisFavoritasMinhaEscola();
